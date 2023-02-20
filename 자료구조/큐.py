a40d488e412b8ca089c8ee6c58549ddab4144d9a
woodong27 <cdw5548@gmail.com>
2023-02-20 21:40:30 +0900

    자료구조 큐 추가

--- 자료구조/큐.py
+++ 자료구조/큐.py
@@ -1,4 +1,18 @@
-que=[]
+q=[]
+q.append(1)
+print(q)
+q.pop(0)
+print(q)
 
-def isempty():
-    return
+from collections import deque
+que=deque([])
+que.append(2)
+print(que)
+que.popleft()
+print(que)
+
+import queue
+qq=queue.Queue()
+qq.put(1)
+print(qq.get())
+print(qq.empty())

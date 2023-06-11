2d7512d0da69b85f56fc73dc036cd4185bda5fa3
woodong27 <122415763+woodong27@users.noreply.github.com>
2023-06-11 23:52:02 +0900

    [D2] Title: 숫자 배열 회전, Time: 112 ms, Memory: 46,376 KB -BaekjoonHub

---
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -0,0 +1,38 @@
+T=int(input())
+
+def rotate_90_degree(list,n):
+    rotated_l=[[0 for x in range(n)]for x in range(n)]
+    for a in range(n):
+        for b in range(n):
+            rotated_l[b][n-1-a]=list[a][b]
+
+    return rotated_l
+
+for x in range(T):
+    N=int(input())
+    l=[list(map(int,input().split()))for _ in range(N)]
+    ans=[[]for _ in range(N)]
+
+    l_90=rotate_90_degree(l,N)
+    l_180=rotate_90_degree(l_90,N)
+    l_270=rotate_90_degree(l_180,N)
+
+    for i in range(N):
+        for j in range(N):
+            ans[i].append(l_90[i][j])
+        ans[i].append(' ')
+
+        for j in range(N):
+            ans[i].append(l_180[i][j])
+        ans[i].append(' ')
+
+        for j in range(N):
+            ans[i].append(l_270[i][j])
+
+    print(f'#{x+1}')
+    for i in range(N):
+        for j in range(len(ans[i])):
+            if j==len(ans[i])-1:
+                print(ans[i][j])
+            else:
+                print(ans[i][j],end='')

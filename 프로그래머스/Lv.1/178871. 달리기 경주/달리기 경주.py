5cdd682c73db84e0507e109f048dbc3df8171913
woodong27 <122415763+woodong27@users.noreply.github.com>
2024-01-11 22:34:59 +0900

    [level Lv.1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,27 @@
+# 프로그래머스 달리기 경주
+
+def solution(players, callings):
+    
+    # 선수 : 등수 딕셔너리 생성
+    ranks = {player : i + 1 for i, player in enumerate(players)}
+    
+    # 등수 : 선수 딕셔너리 생성
+    reversed_ranks = {value : key for key, value in ranks.items()}
+    
+    # 순위 변경을 순서대로 실행
+    for calling in callings:
+        # 추월할 선수의 현재 등수
+        current = ranks[calling]
+        # 추월당한 선수의 이름
+        player = reversed_ranks[current - 1]
+        # ranks에서 추월한 선수 등수 변경
+        ranks[calling] -= 1
+        # reversed_ranks에서 추월한 선수의 등수 변경
+        reversed_ranks[current - 1] = calling
+        # reversed_ranks에서 추월당한 선수의 등수 변경
+        reversed_ranks[current] = player
+        # ranks에서 추월당한 선수의 등수 변경
+        ranks[player] = current
+        
+    answer = list(reversed_ranks.values())
+    return answer

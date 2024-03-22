44879548f1a3f54234f73e2eea149dce9d141e9d
woodong27 <122415763+woodong27@users.noreply.github.com>
2024-03-22 20:18:17 +0900

    [Gold V] Title: ABCDE, Time: 2948 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,37 @@
+# BOJ 13023 G5 ABCDE
+
+import sys
+sys.setrecursionlimit(2000 ** 2)
+
+n, m = map(int, input().split())
+relations = [[] for _ in range(n)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    relations[a].append(b)
+    relations[b].append(a)
+
+
+def dfs(start, friends):
+    global answer
+    if len(friends) == 5:
+        answer = 1
+        return
+
+    for node in relations[start]:
+        if not visited[node]:
+            visited[node] = 1
+            dfs(node, friends + [node])
+            visited[node] = 0
+
+
+answer = 0
+visited = [0] * n
+for i in range(n):
+    visited[i] = 1
+    dfs(i, [i])
+    visited[i] = 0
+    if answer:
+        break
+
+print(answer)

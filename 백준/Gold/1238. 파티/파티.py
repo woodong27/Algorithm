c12f0a2416ea2dc006820ab95bad78b21537a0ea
woodong27 <122415763+woodong27@users.noreply.github.com>
2024-05-18 15:27:56 +0900

    [Gold III] Title: 파티, Time: 60 ms, Memory: 34236 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -4,39 +4,34 @@ import sys
 from heapq import heappush, heappop
 
 
-def dijkstra(start, **kwargs):
-    end = kwargs.get('end', False)
-
+def dijkstra(start, graph):
     q = []
     distance = [float('inf')] * (n + 1)
     distance[start] = 0
     heappush(q, [0, start])
     while q:
         weight, current = heappop(q)
-        if end and current == end:
-            return distance[end]
-
         if distance[current] >= weight:
-            for next_, weight_ in villages[current]:
+            for next_, weight_ in graph[current]:
                 if weight + weight_ < distance[next_]:
                     distance[next_] = weight + weight_
                     heappush(q, [weight + weight_, next_])
 
-    return distance
+    return distance[1:]
 
 
 if __name__ == '__main__':
     enter = sys.stdin.readline
     n, m, x = map(int, enter().strip().split())
     villages = [[] for _ in range(n + 1)]
+    reverse = [[] for _ in range(n + 1)]
     for _ in range(m):
         a, b, c = map(int, enter().strip().split())
         villages[a].append([b, c])
+        reverse[b].append([a, c])
 
-    from_x = dijkstra(x)
-    answer = 0
-    for i in range(1, n + 1):
-        result = dijkstra(i, end=x)
-        answer = max(answer, from_x[i] + result)
+    from_x = dijkstra(x, villages)
+    to_x = dijkstra(x, reverse)
 
+    answer = max([value1 + value2 for value1, value2 in zip(from_x, to_x)])
     print(answer)

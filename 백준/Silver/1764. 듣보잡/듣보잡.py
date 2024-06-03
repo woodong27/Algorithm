c34325b57d97770f2f357fbe1eb7009d6b07c30a
woodong27 <122415763+woodong27@users.noreply.github.com>
2024-06-03 20:56:53 +0900

    [Silver IV] Title: 듣보잡, Time: 72 ms, Memory: 43924 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -6,22 +6,12 @@ import sys
 if __name__ == '__main__':
     enter = sys.stdin.readline
     n, m = map(int, enter().split())
-    heard = [enter().strip() for _ in range(n)]
-    seen = [enter().strip() for _ in range(m)]
 
-    none = {}
-    for person in heard:
-        if person not in none.keys():
-            none[person] = 1
-        else:
-            none[person] += 1
+    heard = set(enter().strip() for _ in range(n))
+    seen = set(enter().strip() for _ in range(m))
 
-    for person in seen:
-        if person not in none.keys():
-            none[person] = 1
-        else:
-            none[person] += 1
+    result = sorted(heard & seen)
+    answer = '\n'.join(result)
 
-    answer = sorted([key for key, value in none.items() if value >= 2])
-    sys.stdout.write(f'{len(answer)}\n')
-    sys.stdout.write('\n'.join(answer))
+    sys.stdout.write(f'{len(result)}\n')
+    sys.stdout.write(f'{answer}\n')

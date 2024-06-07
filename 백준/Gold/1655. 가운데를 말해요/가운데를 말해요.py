099c98a95cc1c7f9400afc5718ba8ec6f3d34f06
woodong27 <122415763+woodong27@users.noreply.github.com>
2024-06-07 21:07:12 +0900

    [Gold II] Title: 가운데를 말해요, Time: 204 ms, Memory: 37132 KB -BaekjoonHub

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -0,0 +1,29 @@
+# BOJ 1655 G2 가운데를 말해요
+
+
+import sys
+from heapq import heappush, heappop
+
+
+if __name__ == '__main__':
+    enter = sys.stdin.readline
+    n = int(enter())
+
+    max_heap = []  # 최대 힙
+    min_heap = []  # 최소 힙
+
+    for _ in range(n):
+        num = int(enter())
+
+        if len(max_heap) == len(min_heap):
+            heappush(max_heap, -num)
+        else:
+            heappush(min_heap, num)
+
+        if min_heap and -max_heap[0] > min_heap[0]:
+            max_val = -heappop(max_heap)
+            min_val = heappop(min_heap)
+            heappush(max_heap, -min_val)
+            heappush(min_heap, max_val)
+
+        sys.stdout.write(f'{-max_heap[0]}\n')

399c608d174ae58053272623f0f2a97cdda50cc6
woodong27 <122415763+woodong27@users.noreply.github.com>
2024-06-17 21:44:33 +0900

    [Gold V] Title: 숫자고르기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -1,44 +1,33 @@
 # BOJ 2668 G5 숫자고르기
 
 import sys
-sys.setrecursionlimit(10000)
 
 
 def dfs(start):
-    global visited, finished, result
+    visited = set()
     stack = []
     current = start
-    while True:
-        if not visited[current]:
-            visited[current] = 1
-            stack.append(current)
-            current = number[current]
-        elif visited[current] and not finished[current]:
-            if current in stack:
-                start = stack.index(current)
-                result.update(stack[start:])
-            break
-        else:
-            break
 
-    while stack:
-        finished[stack.pop()] = 1
+    while current not in visited:
+        visited.add(current)
+        stack.append(current)
+        current = number[current]
+
+    if current in stack:
+        start = stack.index(current)
+        result.update(stack[start:])
 
 
 if __name__ == '__main__':
+    sys.setrecursionlimit(10000)
     enter = sys.stdin.readline
     n = int(enter())
     number = [0] + [int(enter()) for _ in range(n)]
 
-    visited = [0] * (n + 1)
-    finished = [0] * (n + 1)
     result = set()
-
     for i in range(1, n + 1):
-        if not visited[i]:
+        if i not in result:
             dfs(i)
 
-    result = sorted(result)
     sys.stdout.write(f'{len(result)}\n')
-    for num in result:
-        sys.stdout.write(f'{num}\n')
+    print(*sorted(result), sep='\n')
